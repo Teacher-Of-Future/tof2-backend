1445874b5edfa3f0a7711ca8850a48590fbb83aa
David <david@davidcraft.de>
2023-05-05 14:48:34 +0000

    Yeah
    Signed-off-by: GitHub <noreply@github.com>

--- express.js
+++ express.js
@@ -4,10 +4,7 @@ module.exports = async function () {
     console.log("Express module loaded!")
     const app = express()
     app.get('/', function(req, res) {
-        res.send('Hello World!')
-    })
-    app.get('/test', function(req, res) {
-        res.send('Hello World!')
+        res.send('Hello World! Yes it works!')
     })
     app.listen("2023", () => {
         console.log(`Example app listening on port 2023!`)
