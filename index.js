10af82990fb9f285f3e55b95992991661f7e110a
David <david@davidcraft.de>
2023-05-06 14:23:07 +0000

    Test
    Signed-off-by: GitHub <noreply@github.com>

--- index.js
+++ index.js
@@ -5,7 +5,5 @@ console.log("Load express module...");
 require("./express.js")();
 
 console.log("Load database module...");
-const dbDriver = require("./database.js");
-const db = db.openDB();
 
 console.log("Ready!");
