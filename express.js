9496d985187258fe3b488a2dbb20b5cc10b12314
David <david@davidcraft.de>
2023-05-06 14:30:56 +0000

    ok delete sqllite
    Signed-off-by: GitHub <noreply@github.com>

--- express.js
+++ express.js
@@ -1,8 +1,6 @@
 const express = require('express')
 
 module.exports = async function () {
-    const dbDriver = require("./database.js");
-    const db = db.openDB();
     console.log("Express module loaded!")
     const app = express()
     app.get('/', function(req, res) {
@@ -13,9 +11,6 @@ module.exports = async function () {
     })
     app.get('/article', function(req, res) {
         res.send(req.query.id)
-    })
-    app.get('/articles', function(req, res) {
-        res.send(String(db))
     })
     app.listen("2023", () => {
         console.log(`Example app listening on port 2023!`)
