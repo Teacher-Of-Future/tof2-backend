600cb2e753b04cb5b35247aa61e7b6af08baf1a1
David <david@davidcraft.de>
2023-05-06 14:47:57 +0000

    Debug
    Signed-off-by: GitHub <noreply@github.com>

--- database.js
+++ database.js
@@ -1,6 +1,7 @@
 const { sql } = require('@vercel/postgres');
 
 module.exports = async function () {
+    console.log("Database module loaded!")
     const { rows } = await sql`SELECT datname FROM pg_catalog.pg_database;`;
     console.log(rows);
 }
