663b73e99842eb7a1db0ed5a7348e91a00870be8
David <david@davidcraft.de>
2023-05-09 14:36:28 +0000

    So Test Setup
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -8,5 +8,8 @@ connect(config);
 console.log("Ready!");
 test();
 async function test() {
-console.log(await query("SHOW DATABASES;"))
+    console.log(await query("DROP TABLE article;"))
+    console.log(await query("CREATE TABLE article (ID int NOT NULL AUTO_INCREMENT, Title varchar(255) NOT NULL, Katerogie varchar(255) NOT NULL, Makrdown varchar(255) NOT NULL, PRIMARY KEY ID);"))
+    console.log(await query("INSERT INTO article (Title, Katerogie, Makrdown) VALUES ('Test', 'Test', 'Test');"))
+    console.log(await query("SELECT * FROM article;"))
 }
