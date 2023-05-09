57053c9549652cc4bbd24cb91c3b63b4db3633d1
David <david@davidcraft.de>
2023-05-09 14:38:45 +0000

    Update Setup
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -6,10 +6,10 @@ const { connect, query } = require("./database.js");
 connect(config);
 
 console.log("Ready!");
-test();
-async function test() {
-    console.log(await query("DROP TABLE article;"))
+createTable();
+async function createTable() {
+    console.log(await query("DROP TABLE IF EXISTS article;"))
     console.log(await query("CREATE TABLE article (ID int NOT NULL AUTO_INCREMENT, Title varchar(255) NOT NULL, Katerogie varchar(255) NOT NULL, Makrdown varchar(255) NOT NULL, PRIMARY KEY ID);"))
-    console.log(await query("INSERT INTO article (Title, Katerogie, Makrdown) VALUES ('Test', 'Test', 'Test');"))
+    console.log(await query("INSERT INTO article (Title, Katerogie, Makrdown) VALUES ('Hello Word', 'Test', '# Test');"))
     console.log(await query("SELECT * FROM article;"))
 }
