9951f3903174329c1469ba776c8c582ceb119358
David <david@davidcraft.de>
2023-05-10 14:50:37 +0000

    hmm
    Signed-off-by: GitHub <noreply@github.com>

--- setup.js
+++ setup.js
@@ -12,4 +12,9 @@ async function createTable() {
     console.log(await query("CREATE TABLE article (ID int NOT NULL AUTO_INCREMENT, Title varchar(255) NOT NULL, Katerogie varchar(255) NOT NULL, Makrdown varchar(255) NOT NULL, PRIMARY KEY (ID));"))
     console.log(await query("INSERT INTO article (Title, Katerogie, Makrdown) VALUES ('Hello Word', 'Test', '# Test');"))
     console.log(await query("SELECT * FROM article;"))
+
+    console.log(await query("DROP TABLE IF EXISTS question;"))
+    console.log(await query("CREATE TABLE question (ID int NOT NULL AUTO_INCREMENT, Frage varchar(255) NOT NULL, Mail varchar(255), PRIMARY KEY (ID));"))
+    console.log(await query("INSERT INTO question (Frage, Mail) VALUES ('Hello Word', 'Test');"))
+    console.log(await query("SELECT * FROM question;"))
 }
